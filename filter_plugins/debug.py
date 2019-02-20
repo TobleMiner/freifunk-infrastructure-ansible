f47a57f8ba0e121012aaa67f341a1318a4ebff46
Tobias Schramm <tobleminer@gmail.com>
2019-02-20 01:10:06 +0100

    filter_plugins: Add jinja2 debugging filter

---
+++ filter_plugins/debug.py
@@ -0,0 +1,12 @@
+def debug(val, name=None):
+  if name:
+    print('[DEBUG] {}: {}'.format(name,val))
+  else:
+    print('[DEBUG]: {}'.format(val))
+  return ''
+
+class FilterModule(object):
+  def filters(self):
+    return {
+      'debug': debug
+    }
