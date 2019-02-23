b1eb60dc2537175d7e01ba7b9abfd0fc3df59b64
Tobias Schramm <tobleminer@gmail.com>
2019-02-23 16:40:32 +0100

    filter_plugins: Add sha256 filter

--- filter_plugins/hashnum.py
+++ filter_plugins/hashnum.py
@@ -1,19 +1,23 @@
 import hashlib
 
-def sha256(s):
+def sha256digest(s):
   return hashlib.sha256(str(s).encode('utf-8'))
 
 def hashnum(s, min, max):
-  return int(sha256(s).hexdigest(), 16) % (max - min) + min
+  return int(sha256digest(s).hexdigest(), 16) % (max - min) + min
 
 def hashipv4(s):
-  h = sha256(s).digest()
+  h = sha256digest(s).digest()
   parts = [ str(int.from_bytes(h[i:i + 1], 'little')) for i in range(4) ]
   return '.'.join(parts)
+
+def sha256(s):
+  return sha256digest(s).hexdigest()
 
 class FilterModule(object):
   def filters(self):
     return {
       'hashnum': hashnum,
       'hashipv4': hashipv4,
+      'sha256': sha256,
     }
